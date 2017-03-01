75970ce4133862c3467d47019010bbe39e7faf9e
M. Murat Kan <gusmehmka@student.gu.se>
2017-03-01 20:55:22 +0100

    updated sortedlist

--- SortedList.java
+++ SortedList.java
@@ -1,180 +1,87 @@
 import java.util.Arrays;
 
-public class SortedList<E extends Comparable<E>> {
-
-
+public class SortedListImpl<E extends Comparable<E>> implements SortedList<E> {
+    
     private int arraysize = 0;
-
-
-    @SuppressWarnings("unchecked")
-
-    private E[] arr = (E[]) new Comparable[arraysize];
-
-
-
+    private int size = 0;
+    private E[] arr = (E[]) new Comparable[size];
     public void add(E elem) {
-
-
-        if (arraysize <= arr.length) {
-
-            arraysize++;
-
-            arr = Arrays.copyOf(arr, arraysize);
-
+        if (size <= 0) {
+            size++;
+        } else if(arraysize>=arr.length){
+            size = arraysize * 2;
         }
-
-
-        arr[arraysize - 1] = elem;
-
-
-        int lo = 0;
-
-        int hi = arraysize - 1;
-
-
-        while (lo <= hi) {
-
-
-            int mid = (lo + hi) / 2;
-
-
-            if (elem.compareTo(arr[mid]) > 0) {
-
-                lo = mid + 1;
-
-            } else if (elem.compareTo(arr[mid]) < 0) {
-
-                hi = mid - 1;
-
-            } else {
-
-                lo = mid;
-
-                break;
-
-            }
-
-
-        }
-
-
-        E tmpE = arr[lo];
-
-        arr[lo] = elem;
-
-
-        for (int i = lo + 1; i <= arraysize - 1; i++) {
-
-            arr[arraysize - 1] = arr[i];
-
-            arr[i] = tmpE;
-
-            tmpE = arr[arraysize - 1];
-
-        }
-
+        arr = Arrays.copyOf(arr, size);
+        
+        arr[arraysize] = elem;
+        arraysize++;
+        swap(elem, searchPos(elem, true));
     }
-
-
-
+    
     public void addSortedArray(E[] arr) {
-
-        for (E elem : arr) {
-
+        for (E elem : arr)
             add(elem);
-
-        }
-
     }
-
-
-
+    
     public E get(int ix) {
-
+        if (ix < 0 || ix >= arraysize)
+            return null;
         return arr[ix];
-
     }
-
-
+    
     public int firstIndex(E elem) {
-
-        int firstIndex = 0;
-
-        for (int i = 0; i < arraysize; i++) {
-
-            if (elem.compareTo(get(i)) <= 0) {
-
-                firstIndex = i;
-
-                break;
-
-            } else {
-
-                firstIndex++;
-
-            }
-
-        }
-
-        return firstIndex;
-
+        return searchPos(elem, true);
     }
-
-
-
-
+    
     public int lastIndex(E elem) {
-
-        int lastIndex = 0;
-
-        for (int i = 0; i < arraysize; i++) {
-
-            if (elem.compareTo(get(i)) < 0) {
-
-                lastIndex = i - 1;
-
-                break;
-
-            } else {
-
-                lastIndex = i;
-
-            }
-
-        }
-
-        return lastIndex;
-
+        return searchPos(elem, false);
     }
-
-
+    
     public boolean contains(E elem) {
-
-        for (int i = 0; i < arraysize - 1; i++) {
-
-            if (elem.compareTo(get(i)) == 0) {
-
-                return true;
-
-            }
-
-        }
-
-        return false;
-
+        return elem.compareTo(arr[searchPos(elem, true)]) == 0;
     }
-
+    
     public int countBetween(E lo, E hi) {
-
         return lastIndex(hi) - firstIndex(lo) + 1;
-
     }
-
+    
     public int size() {
-
         return arraysize;
-
     }
-
-
+    
+    private int searchPos(E elem, boolean lower) {
+        int lo = 0;
+        int hi = arraysize - 1;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (lower) {
+                if (elem.compareTo(arr[mid]) > 0) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                if (elem.compareTo(arr[mid]) < 0) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }		
+        return lower?lo:hi;
+    }
+    
+    private E[] swap(E elem, int lo) {
+        E tmpE = arr[lo];
+        arr[lo] = elem;
+        
+        for (int i = lo + 1; i <= arraysize - 1; i++) {
+            arr[arraysize - 1] = arr[i];
+            arr[i] = tmpE;
+            tmpE = arr[arraysize - 1];
+        }
+        return arr;
+    }
+    
 }
